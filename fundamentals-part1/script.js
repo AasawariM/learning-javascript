20048468c5aeb6e1b2e0241c0865196d2b7c8c15
AasawariM <aasawari.m.work@gmail.com>
2025-12-19 12:59:50 +0530

    variables in js

--- fundamentals-part1/script.js
+++ fundamentals-part1/script.js
@@ -1,8 +1,62 @@
 let js = "amazing";
-if (js == "amazing") {
-  alert("Javascript is Amazing!!");
-} else {
-  alert("bye bye js!");
-}
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+// value is a piece of data(fundamental unit of information in programming)
+
+console.log('Steve');
+console.log(35);
+
+// we can store values in variables to use them again and again
+// below we are declaring a variable
+// it will create a new variable in a computer's memory and store value in that variable
+
+let fName = "Steve harrington";
+console.log(fName); //camel case = standard way
+let first_name = "Jonathan"; //can also write like this
+
+//cannot start with numbers
+// let 3years = 3
+// all the errors we make will end up always in console without using console.log().
+
+// let john&harry = 'jh'; cannot use '&'
+
+// variables can only contain letters,numbers,underscore and $ sign.
+let john$harry = 'jh';
+let john_harry = 'jh';
+let j7h8 = 'jh';
+
+// cannot use reserved js keywords
+// let new = 27;
+// let function = "abc";
+// let switch = "abc";
+
+// only 2 symbols allowed besides letters and numbers
+let _function = "abc";
+let $switch = 4;
+
+// name = a kind of reserved keyword but legal to use = thus, dont use 
+
+// let name = "john";
+// console.log(name);
+
+// should not use uppercase also to start a variable name
+// convention thus not illegal 
+// but used in specific use case like OOP in js
+// let Person = "Ram";
+
+// therefore use it like this = lowercase starting
+let person = "Ram";
+
+// variables that are fully uppercase are reserved for constants that we know will never change.
+let PI = 3.1415;
+
+// final naming convention = make sure variable names are descriptive (easy to understand what value it holds just by reading a name of variable).
+
+let myFirstPetName = "Tommy";
+let myCurrentPetName = "Scotch";
+
+// above is better than below
+
+let pet1 = "Tommy";
+let pet2 = "Scotch";
+
