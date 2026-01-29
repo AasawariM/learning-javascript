b58cee08933548336b3f94855cae230e78816997
AasawariM <aasawari.m.work@gmail.com>
2026-01-29 20:13:24 +0530

    selecting and manipulating elements

--- DOM/guess_my_number/index.js
+++ DOM/guess_my_number/index.js
@@ -13,3 +13,22 @@ console.log(document.querySelector('.number')); //this selects the entire elemen
 // we want to access the actual number value currently is ?
 // to do so we have textcontent property
 console.log(document.querySelector('.number').textContent);
+
+// along with getting the text content we can set the content of the element also.
+// here we manipulated the text content of one of the DOM nodes.
+document.querySelector('.message').textContent = "🎉 Correct Number!";
+
+// setting random number from 1 to 20 instead of ? 
+document.querySelector('.number').textContent = Math.floor(Math.random() * 20) + 1;
+
+// same way for score 
+document.querySelector('.score').textContent = 10;
+
+// same way of input field
+// but instead of text content here we will need a value so will use value property.
+
+// to get value from input field 
+console.log(document.querySelector('.guess').value); //nothing currently
+
+// to set value to the input field
+console.log(document.querySelector('.guess').value = 20); //prints 20
