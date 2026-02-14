8039029725182058f5ce564f56011ed06e169a3f
AasawariM <aasawari.m.work@gmail.com>
2026-02-14 21:35:05 +0530

    scoping in js

--- Behind-the-Scenes/script.js
+++ Behind-the-Scenes/script.js
@@ -1,29 +1,53 @@
 'use strict';
 function calcAge(birthyear) {
   const age = 2026 - birthyear;
-  // creating another function inside of this function
   function printAge() {
-    //this function will create a new scope
-    const output = `${firstName},  you are ${age},born in ${birthyear}`;
+    // here the firstName will be jonas and not steven bcz it is not in the current scope of this function so js will do variable lookup.
+    let output = `${firstName},  you are ${age},born in ${birthyear}`;
     console.log(output);
+    // creating a block scope in printAge Function
+
+    if (birthyear > 1981 && birthyear <= 1996) {
+      var millenial = true;
+      //creating new variable with same name as outer scope
+      const firstName = 'steven'; // what will be dsplayed here jonas or steven?
+      // will got steven bcz js will always first look in current scope and we found
+      //  this here so js engine will not perform any variable look up in scope chain.
+
+      // *** so scope chain isnt neccessary at all if the variable we are looking for is already in current scope.
+      const str = `Oh, and you're a millenial,${firstName}`; //lookup for firstName
+      console.log(str);
+
+      // lets prove that functions are also block scoped starting with ES6
+      // function to add two values
+      function add(a, b) {
+        return a + b;
+      }
+      // reassigning outer scope's variable
+      output = 'New OUTPUT!'; //redefining variable from outer scope in this child scope.
+      // and if we try to access it below then
+    }
+    // if tried this outside the block as its a const and const let variables are block scoped
+    // console.log(str);
+    // old pre ES6 variable
+    console.log(millenial); //js can find var bcz var variables are function scoped (trying to access the variable that is defined in same function) so they ignore block.
+    // calling the add function outside the block
+    // console.log(add(2, 3)); //reference error add not defined
+    // bcz scope of this add function is onlythe block in which it is defined,but
+    // this is only true for strict mode.
+    console.log(output); //bcz we manipulate an exisitng variable inside a child/inner scope.
+    // we arent creating new variable ,we simply redefine a variable by accessing it from parent scope.
   }
-  printAge(); //as we are executing the printAge function, js engine is trying to access/find the age variable in
-  // the current scope as it is not in current scope so it goes to parent scope where it does found the age variable.
-  //same true for birthyear variable because for scoping the parameter of a function work just like normal variables
+  printAge();
   return age;
 }
 const firstName = 'jonas';
 calcAge(1991);
-// age wont be accessible here as it is accessible only inside the function
-// console.log(age);
-// scope chain is always one directional
-// only an inner scope can have access to the variables of its outer scope
-// , not the other way around
-// same for functions
-// we cannot do
-// printAge();
 
-// so here in global scope we dont have access to any variables defined in any other scope.
-// firstName variable is also accessed in printAge function by doing variable lookup in scope chain.
-// js engine first checks in current scope, not found,so looks up in scope chain
-//  which is the calcAge function its also not there then it goes even further i.e in global scope and its there.
+// note that
+// you can have multiple variables of same name in different scopes as they are completely different variables happen to have same name
+// no problem of same name bcz they have defined in different scopes so you can have repeated variable names.
+// thats also a reason why you have different functions with same parameter names.bcz each parameter is only defined in that scope of that function.
+// i.e why there is not problem at all to have many functions with the exact same parameter names in the same way there is not a problem
+// to have functions with same variable names inside of them.
+// what happen if we reassgin the output variable inside block
