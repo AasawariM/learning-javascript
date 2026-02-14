386347f77d45e56a31e26ab8f1f5858972eb492a
AasawariM <aasawari.m.work@gmail.com>
2026-02-14 19:29:23 +0530

    variable look up

--- Behind-the-Scenes/script.js
+++ Behind-the-Scenes/script.js
@@ -1,26 +1,29 @@
 'use strict';
-
-// function calcAge declaration
-// defined in global scope bcz it is in top-level code
-// this function here creates its own scope which will be equivalent to the varible environment of its exection context.
 function calcAge(birthyear) {
   const age = 2026 - birthyear;
-  console.log(firstName); //this variable is not actually in this function scope however it is global variable
-  // through the scope chain its gonna be made available inside this function.
+  // creating another function inside of this function
+  function printAge() {
+    //this function will create a new scope
+    const output = `${firstName},  you are ${age},born in ${birthyear}`;
+    console.log(output);
+  }
+  printAge(); //as we are executing the printAge function, js engine is trying to access/find the age variable in
+  // the current scope as it is not in current scope so it goes to parent scope where it does found the age variable.
+  //same true for birthyear variable because for scoping the parameter of a function work just like normal variables
   return age;
 }
-
-// creating a global variable
 const firstName = 'jonas';
-// calling the function
 calcAge(1991);
+// age wont be accessible here as it is accessible only inside the function
+// console.log(age);
+// scope chain is always one directional
+// only an inner scope can have access to the variables of its outer scope
+// , not the other way around
+// same for functions
+// we cannot do
+// printAge();
 
-// when js executed this line
-// console.log(firstName);
-// js didnt find this variable in the function scope so it did a variable lookup where it looked up
-// in the scope chain to see if it found the variable there,and as the parent scope of the calcAge function is the global scope and firstName variable is there.
-// even though the variable is defined after calcAge function
-// but thats not a problem bcz code in the function is only executed when its called.
-// and that  happens after declration of firstName variable.
-// so this variable is already in global execution variable enviornment
-// so global scope is ready to be used.
+// so here in global scope we dont have access to any variables defined in any other scope.
+// firstName variable is also accessed in printAge function by doing variable lookup in scope chain.
+// js engine first checks in current scope, not found,so looks up in scope chain
+//  which is the calcAge function its also not there then it goes even further i.e in global scope and its there.
